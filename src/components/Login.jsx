d48e192b18d28f4ca996e5b213785b049acbae9f
srighankittu <107845663+srighankittu@users.noreply.github.com>
2024-02-19 11:00:04 +0530

    Sign in and Sign Up forms created

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,9 @@
 import Header from "./Header";
 import banner from "/assets/banner.webp";
+import { useState } from "react";
 
 function Login() {
+  const [isSignIn, setIsSignIn] = useState(true);
   return (
     <div>
       <div>
@@ -10,7 +12,18 @@ function Login() {
       <img className="absolute" src={banner} alt="body" />
 
       <form className="w-3/12 absolute z-10 bg-black my-60 mx-auto right-0 left-0 bg-opacity-60 p-12 text-white rounded-lg">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn ? (
+          <input
+            type="text"
+            placeholder="Enter Full Name"
+            className="w-full p-2 my-2 bg-gray-800 "
+          />
+        ) : (
+          ""
+        )}
         <input
           type="text"
           placeholder="Email"
@@ -22,9 +35,16 @@ function Login() {
           className="w-full p-2 my-2 bg-gray-800"
         />
         <button className="w-full p-2 my-2 bg-red-800 rounded-lg">
-          Sign In!
+          {isSignIn ? "Sign In!" : "Sign Up!"}
         </button>
-        <p className="py-4">New to Netflix? Sign Up Now!</p>
+        <p
+          className="py-4 cursor-pointer"
+          onClick={() => setIsSignIn(!isSignIn)}
+        >
+          {isSignIn
+            ? "New to Netflix? Sign Up Now!"
+            : "Already a user? Sign in Now!"}
+        </p>
       </form>
     </div>
   );
