72ab6102ee138623cc04bf12b9b4fd0ec02f2776
srighankittu <107845663+srighankittu@users.noreply.github.com>
2024-03-13 14:21:17 +0530

    Removed backend here so that i can initialise it in a different repo

--- server/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  MONGOOSE_URL:
-    "mongodb+srv://srighan_practice:oiiQGcWEg8ASqIsq@cluster0.yymynan.mongodb.net/streamverse",
-  JWT_SECRET:
-    "lladsmk023mlkdwmsdsdasdasdasdeewasdasdewawdadsadq3wadsasdasdasdasfagfsasdaxz0",
-};
-
-export default config;
