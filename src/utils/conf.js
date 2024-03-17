ad660597da0d3effb750871abe97ea8cdf736683
srighankittu <107845663+srighankittu@users.noreply.github.com>
2024-03-17 12:02:50 +0530

    Config changes

--- src/utils/conf.js
+++ src/utils/conf.js
@@ -1,8 +1,15 @@
-export const LATEST_MOVIES = "http://localhost:8000/user/latestMovies";
-export const VIDEO_DATA = "http://localhost:8000/user/video/";
-export const SIGN_IN = "http://localhost:8000/user/signin";
-export const SIGN_UP = "http://localhost:8000/user/signup";
-export const POPULAR_MOVIES = "http://localhost:8000/user/popularmovies";
-export const TOP_RATED_MOVIES = "http://localhost:8000/user/topratedmovies";
-export const UPCOMING_MOVIES = "http://localhost:8000/user/upcomingmovies";
+export const LATEST_MOVIES =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/latestMovies";
+export const VIDEO_DATA =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/video/";
+export const SIGN_IN =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/signin";
+export const SIGN_UP =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/signup";
+export const POPULAR_MOVIES =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/popularmovies";
+export const TOP_RATED_MOVIES =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/topratedmovies";
+export const UPCOMING_MOVIES =
+  "https://stream-verse-ott-backend-production.up.railway.app/user/upcomingmovies";
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w300/";
